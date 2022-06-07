93e4d6e4cd926efc846f8f888f66bc3090a0c458
a1ef0 <a1ef0@github.com>
2022-06-07 22:01:20 +0300

    BUGFIX

--- src/Block.hs
+++ src/Block.hs
@@ -1,13 +1,15 @@
 module Block where
 
 data Block = Block PrevHash MinerHash Nonce TransCount TransList
-data Transaction = [(SenderHash, RecvHash, Amount)]
+
+data Transaction = Transaction [(SenderHash, RecvHash, Amount)]
 
 type SenderHash = BlockHash
 type RecvHash = BlockHash
 type Amount = Double
 type PrevHash = BlockHash
 type MinerHash = BlockHash
+type BlockHash = Integer
 type Nonce = Integer
 type TransCount = Integer
 type TransList = [Transaction]
