b21810442a9bd71f1b1f8d48680f7679366b4c91
a1ef0 <a.buchnev@innopolis.university>
2022-06-16 23:43:24 +0300

    minor changes

--- src/Explorer.hs
+++ src/Explorer.hs
@@ -9,17 +9,29 @@ import TBlock
 
 getEnumeratedTransList :: Integer -> [Transaction] -> String
 getEnumeratedTransList _ [] = ""
-getEnumeratedTransList num ((Transaction senderHash recvHash amount _signature): xs) = "~~~~~~~~~~~~~~~\nTransaction number: " ++ (show num) ++ "\nSender Hash: " ++ (show senderHash) ++ "\nRecv Hash: " ++ (show recvHash) ++ "\nAmount: " ++ (show amount) ++ "\n" ++ (getEnumeratedTransList (num + 1) xs)
+getEnumeratedTransList num ((Transaction senderHash recvHash amount _signature): xs) 
+    = "~~~~~~~~~~~~~~~\nTransaction number: " ++ (show num) 
+    ++ "\nSender Hash: " ++ (show senderHash) 
+    ++ "\nRecv Hash: "   ++ (show recvHash) 
+    ++ "\nAmount: "      ++ (show amount) 
+    ++ "\n"              ++ (getEnumeratedTransList (num + 1) xs)
 
 getTransList :: [Transaction] -> String
 getTransList transList = getEnumeratedTransList 1 transList
 
 getBlockInfo :: Block -> String
-getBlockInfo (Block prevHash minerHash nonce transCount transList) = "Prev Hash: " ++ (show prevHash) ++ "\nMiner Hash: " ++ (show minerHash) ++ "\nNonce: " ++ (show nonce) ++ "\nTrans Count: " ++ (show transCount) ++ "\nTransactions: \n" ++ (getTransList transList)
+getBlockInfo (Block prevHash minerHash nonce transCount transList) 
+    = "Prev Hash: "         ++ (show prevHash) 
+    ++ "\nMiner Hash: "     ++ (show minerHash) 
+    ++ "\nNonce: "          ++ (show nonce) 
+    ++ "\nTrans Count: "    ++ (show transCount) 
+    ++ "\nTransactions: \n" ++ (getTransList transList)
 
 sumBlocksInfo :: [Block] -> String
 sumBlocksInfo [] = "\n"
-sumBlocksInfo (x: xs) = (getBlockInfo x) ++ "##################\n" ++ sumBlocksInfo xs 
+sumBlocksInfo (x: xs) 
+    = (getBlockInfo x) ++ "##################\n" 
+    ++ sumBlocksInfo xs 
 
 exploreNetwork :: Handler
 exploreNetwork state = (putStrLn $ sumBlocksInfo $ blocks $ state, Just state)
