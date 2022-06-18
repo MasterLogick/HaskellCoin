04beb4094ef015734b51891419cea539f22ebb6a
ananastya1 <nastya10_tv@mail.ru>
2022-06-19 00:08:40 +0300

    Add help

--- src/HelpCommand.hs
+++ src/HelpCommand.hs
@@ -4,4 +4,10 @@ import MinerState
 
 printHelp :: Handler
 printHelp _ = do
-    putStrLn "HERE WILL BE HELP"
+    putStrLn "exit - exit from programm"
+    putStrLn "build - build new block and send it to network"
+    putStrLn "show - print chain of blocks"
+    putStrLn "commit [senderHash] [recieverHash] [amountMoney] - create transaction"
+    putStrLn "connect [ip] [port] - connect to server at specified ip and port"
+    putStrLn "balance [userHash] - print user's balance"
+    putStrLn "start server [ip] [port] - start server at specified ip and port"
