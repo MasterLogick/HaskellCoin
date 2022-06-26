e1069c5c596f0ffc3cec40241ed4e42778fb11c2
NAD777 <30959736+NAD777@users.noreply.github.com>
2022-06-26 19:56:41 +0300

    Get transaction list of user and balance

--- src/AccountBalance.hs
+++ src/AccountBalance.hs
@@ -6,26 +6,50 @@ import MinerState
 import TBlock
 
 
--- | Getting of transaction's balance.
-getTransactionBalance :: SenderHash -> Transaction -> Amount
-getTransactionBalance id_sender (Transaction senderHash recvHash amount _) 
- | id_sender == senderHash = (-amount)
- | id_sender == recvHash = amount
- | otherwise = 0
+-- | Checks if id_sender in this transaction
+getTransaction :: SenderHash -> Transaction -> TransList
+getTransaction id_sender (Transaction senderHash recvHash amount sign) 
+ | id_sender == senderHash = [(Transaction senderHash recvHash amount sign)]
+ | id_sender == recvHash = [(Transaction senderHash recvHash amount sign)]
+ | otherwise = []
 
- -- | Getting of block's balance.
-getBalanceBlock :: SenderHash -> TransList -> Amount
-getBalanceBlock _ [] = 0
-getBalanceBlock id_sender (transition: transList) = getTransactionBalance id_sender transition + getBalanceBlock id_sender transList
+ -- | Getting transaction of user from transaction list
+getTransactionsBlock :: SenderHash -> TransList -> TransList
+getTransactionsBlock _ [] = []
+getTransactionsBlock id_sender (transition: transList) = getTransaction id_sender transition ++ getTransactionsBlock id_sender transList
 
--- | Getting transactions' balance of user with usage of blocks' array.
-getBalance :: SenderHash -> [Block] -> Amount
+-- | Getting transactions' list of user with usage of blocks' array.
+getListTransactions :: SenderHash -> [Block] -> TransList
+getListTransactions _ [] = []
+getListTransactions id_sender ((Block prevHash minerHash nonce transCount transList): xs) = getTransactionsBlock id_sender transList ++ getListTransactions id_sender xs
+
+-- | Get user balance from transaction list
+getBalance :: SenderHash -> TransList -> Amount
 getBalance _ [] = 0
-getBalance id_sender ((Block prevHash minerHash nonce transCount transList): xs) = getBalanceBlock id_sender transList + getBalance id_sender xs
+getBalance id_sender ((Transaction senderHash recvHash amount _): xs) = amount' + getBalance id_sender xs
+ where
+    amount'
+      | id_sender == senderHash = -amount 
+      | id_sender == recvHash = amount
+      | otherwise = 0
+      
+
+prettyTransactions :: SenderHash -> TransList -> String
+prettyTransactions _ [] = ""
+prettyTransactions id_sender ((Transaction senderHash recvHash amount sign) : xs) = result ++ (prettyTransactions id_sender xs)
+  where
+    result 
+      | id_sender == senderHash = (show senderHash) ++ " -> " ++ (show recvHash) ++ " | " ++ (show amount) ++ "\n"
+      | id_sender == recvHash = (show recvHash) ++ " <- " ++ (show senderHash) ++ " | " ++ (show amount) ++ "\n"
+      | otherwise = ""
+
 
 -- | Getting transactions' balance of user.
 userBalance :: SenderHash -> Handler
 userBalance id_sender stateRef = do
     miner <- readMVar stateRef
-    putStrLn (show $ getBalance id_sender (blocks miner))
+    let transactions = getListTransactions id_sender (blocks miner)
+    putStrLn("From:                                       To:                                        Amount:")
+    putStrLn (prettyTransactions id_sender transactions)
+    putStrLn ("Balance: " ++ (show $ getBalance id_sender transactions))
     return ()
